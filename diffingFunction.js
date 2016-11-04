b2f528c2992140bc8598682a620348eea364b2d5
Coral He <che@klick.com>
2016-11-04 17:17:39 -0400

    removed some crap from webites

--- diffingFunction.js
+++ diffingFunction.js
@@ -1,3 +1,14 @@
+ /*
+ * Javascript Diff Algorithm
+ *  By John Resig (http://ejohn.org/)
+ *  Modified by Chu Alan "sprite"
+ *
+ * Released under the MIT license.
+ *
+ * More Info:
+ *  http://ejohn.org/projects/javascript-diff-algorithm/
+ */
+
 function escape(s) {
     var n = s;
     n = n.replace(/&/g, "&amp;");
